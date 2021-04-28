8af77709ceb107874a6c8c162fed1454d8f42f8d
Enock Kasaadha <enocksdha@gmail.com>
2021-04-28 12:32:57 +0300

    created placeholder components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
-const element = (
-    <div title="Outer div">
-        <h1>Hello, World!</h1>
-    </div>
-    ); ReactDOM.render(element, document.getElementById('contents'))
+class IssueFilter extends React.Component{
+    render(){
+        return(
+            <div>this is a placeholder for the issue filter</div>
+        )
+    }
+}
+
+class IssueTable extends React.Component{
+    render(){
+        return(
+            <div>this is a placeholder for the issue Table</div>
+        )
+    }
+}
+
+class IssueAdd extends React.Component{
+    render(){
+        return(
+            <div>this is a placeholder for a form to add an issue</div>
+        )
+    }
+}
+
+class IssueList extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <h1>Issue Tracker</h1>
+                <IssueFilter/>
+                <hr/>
+                <IssueTable/>
+                <hr/>
+                <IssueAdd/>
+            </React.Fragment>
+        )
+    }
+}
+
+ReactDOM.render(<IssueList/>, document.getElementById('contents'))
