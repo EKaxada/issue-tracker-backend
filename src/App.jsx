8729157f58338b151077e4291947972ddc58570f
Enock Kasaadha <enocksdha@gmail.com>
2021-04-28 13:25:52 +0300

    updated state with sample data

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,11 @@ const initialIssues = [
   },
 ];
 
+const sampleIssue = {
+    status: 'New', owner: 'Pieta',
+    title: 'Completion date should be optional',
+  };
+
 class IssueFilter extends React.Component {
   render() {
     return <div>this is a placeholder for the issue filter</div>;
@@ -46,6 +51,17 @@ class IssueTable extends React.Component {
   constructor() {
     super();
     this.state = { issues: [] };
+    setTimeout(() => {
+        this.createIssue(sampleIssue)
+    }, 2000);
+  }
+
+  createIssue(issue) {
+      issue.id = this.state.issues.length+1;
+      issue.created = new Date();
+      const newIssueList = this.state.issues.slice();
+      newIssueList.push(issue);
+      this.setState({issues: newIssueList})
   }
 
   componentDidMount() {
