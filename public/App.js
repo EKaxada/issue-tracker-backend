4c35c14b2b69e80c6a3f7a80105e53c46b1dcd18
Enock Kasaadha <enocksdha@gmail.com>
2021-04-28 12:18:07 +0300

    Presets configured for JSX and ES5 trasform

--- public/App.js
+++ public/App.js
@@ -1,4 +1,6 @@
-const element = /*#__PURE__*/React.createElement("div", {
+"use strict";
+
+var element = /*#__PURE__*/React.createElement("div", {
   title: "Outer div"
 }, /*#__PURE__*/React.createElement("h1", null, "Hello, World!"));
 ReactDOM.render(element, document.getElementById('contents'));
