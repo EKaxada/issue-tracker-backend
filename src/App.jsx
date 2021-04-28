52552e462b1adf2449297b9ad5bd561fd8c9dd89
Enock Kasaadha <enocksdha@gmail.com>
2021-04-28 13:12:05 +0300

    added dynamic composition

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
+const issues = [
+  {
+    id: 1,
+    status: "New",
+    owner: "Ravan",
+    effort: 5,
+    created: new Date("2018-08-15"),
+    due: undefined,
+    title: "Error in console when clicking Add",
+  },
+  {
+    id: 2,
+    status: "Assigned",
+    owner: "Eddie",
+    effort: 14,
+    created: new Date("2018-08-16"),
+    due: new Date("2018-08-30"),
+    title: "Missing bottom border on panel",
+  },
+];
+
 class IssueFilter extends React.Component {
   render() {
     return <div>this is a placeholder for the issue filter</div>;
@@ -6,11 +27,16 @@ class IssueFilter extends React.Component {
 
 class IssueRow extends React.Component {
   render() {
-    const style = this.props.rowStyle;
+    const { id, status, owner, created, effort, due, title } = this.props.issue;
     return (
       <tr>
-          <td style={style}>{this.props.issue_id}</td>
-        <td style={style}>{this.props.children}</td>
+        <td>{id}</td>
+        <td>{status}</td>
+        <td>{owner}</td>
+        <td>{created.toDateString()}</td>
+        <td>{effort}</td>
+        <td>{due ? due.toDateString() : ""}</td>
+        <td>{title}</td>
       </tr>
     );
   }
@@ -19,22 +45,23 @@ class IssueRow extends React.Component {
 class IssueTable extends React.Component {
   render() {
     const rowStyle = { border: "1px solid silver", padding: 4 };
+    const issueRows = issues.map((issue) => (
+      <IssueRow rowStyle={rowStyle} issue={issue} />
+    ));
     return (
       <table style={{ borderCollapse: "collapse" }}>
         <thead>
           <tr>
-            <th style={rowStyle}>ID</th>
-            <th style={rowStyle}>Title</th>
+            <th>ID</th>
+            <th>Status</th>
+            <th>Owner</th>
+            <th>Created</th>
+            <th>Effort</th>
+            <th>Due Date</th>
+            <th>Title</th>
           </tr>
         </thead>
-        <tbody>
-         <IssueRow rowStyle={rowStyle} issue_id={1}>
-             Error in console when clicking Add
-         </IssueRow>
-         <IssueRow rowStyle={rowStyle} issue_id={2}>
-             <div>Missing <b>bottom</b> border on panel</div>
-         </IssueRow>
-        </tbody>
+        <tbody>{issueRows}</tbody>
       </table>
     );
   }
