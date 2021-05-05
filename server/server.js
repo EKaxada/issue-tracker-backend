800252711410ed6ce4091758ac21239be7c9ff56
Enock Kasaadha <enocksdha@gmail.com>
2021-05-05 09:50:16 +0300

    initialized graphql API schema

--- server/server.js
+++ server/server.js
@@ -1,10 +1,46 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express"); // import apollo server object
+
+let aboutMessage = "Issue Tracker API v1.0";
+
+// Schema definition using String literals
+const typeDefs = `
+    type Query {
+        about: String!
+    }
+    type Mutation {
+        setAboutMessage(message: String!): String
+    }
+`;
+
+// handler functions to resolve queries with real values
+const resolvers = {
+    Query: {
+        about: () => aboutMessage,
+    },
+    Mutation: {
+        setAboutMessage,
+    },
+};
+
+function setAboutMessage(_, { message }) {
+    return (aboutMessage = message);
+}
+
+// construction of apollo server with two properties and return a GraphQL server object
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+const app = express();
 
 // serve the public folder to the home route '/'
-app.use('/', express.static('public'))
+app.use("/", express.static("public"));
 
-// start application 
+server.applyMiddleware({ app, path: "/graphql" }); // install apollo_server as middleware in express
+
+// start application
 app.listen(3000, () => {
-    console.log('App started on port 3000')
-})
+    console.log("App started on port 3000");
+});
