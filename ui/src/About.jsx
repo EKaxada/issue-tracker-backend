ad8e4ce6f144e381f60ad35ffa04a5e5dc057658
EKaxada <enocksdha@gmail.com>
2021-05-11 07:54:55 +0300

    .gitignore fix

--- ui/src/About.jsx
+++ ui/src/About.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import store from './store.js'
 
-export default function About(){
-    return(
+export default class About extends React.Component(){
+
+    constructor(props) {
+        super(props);
+        const apiAbout =store.initialData?store.initialData.about:null;
+        this.state = {apiAbout}
+    }
+
+   render() {return(
         <div className="text-center">
             <h3>Issue Tracker version 0.9</h3>
             <h4>
                 {store.initialData ? store.initialData.about : 'unknown'} 
             </h4>
         </div>
-    )
+    )}
 }
