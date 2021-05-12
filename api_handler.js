5382bd4319125b3df62cbf8e9b7d7191231a2064
EKaxada <enocksdha@gmail.com>
2021-05-12 09:48:58 +0300

    changes for heroku

--- api_handler.js
+++ api_handler.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
-require('dotenv').config();
-const { ApolloServer } = require('apollo-server-express');
+const fs = require("fs");
+require("dotenv").config();
+const { ApolloServer } = require("apollo-server-express");
 
-const GraphQLDate = require('./graphql_date.js');
-const about = require('./about.js');
-const issue = require('./issue.js');
+const GraphQLDate = require("./graphql_date.js");
+const about = require("./about.js");
+const issue = require("./issue.js");
 
 const resolvers = {
   Query: {
@@ -22,18 +22,20 @@ const resolvers = {
 };
 
 const server = new ApolloServer({
-  typeDefs: fs.readFileSync('schema.graphql', 'utf-8'),
+  typeDefs: fs.readFileSync("schema.graphql", "utf-8"),
   resolvers,
   formatError: (error) => {
     console.log(error);
     return error;
   },
+  playground: true,
+  introspection: true,
 });
 
 function installHandler(app) {
-  const enableCors = (process.env.ENABLE_CORS || 'true') === 'true';
-  console.log('CORS setting:', enableCors);
-  server.applyMiddleware({ app, path: '/graphql', cors: enableCors });
+  const enableCors = (process.env.ENABLE_CORS || "true") === "true";
+  console.log("CORS setting:", enableCors);
+  server.applyMiddleware({ app, path: "/graphql", cors: enableCors });
 }
 
 module.exports = { installHandler };
